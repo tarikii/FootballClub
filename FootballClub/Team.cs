405de0361fb8f115b43eaf822f5786ff8192157e
tarikii <tarikaabouchazougarh@gmail.com>
2024-06-04 12:16:09 +0200

    Team class created

---
+++ FootballClub/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballClub
+{
+    public class Team
+    {
+        string teamName;
+
+        public string TeamName { get; }
+        public List<Player> Players { get; }
+
+        public Team(string teamName)
+        {
+            this.teamName = teamName;
+            Players = new List<Player>();
+        }
+    }
+}
